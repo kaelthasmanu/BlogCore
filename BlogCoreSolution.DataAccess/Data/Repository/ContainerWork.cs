459cfd23e20b1f3167aba7441036c88e7061a900
Manuel <manuelalberto.gorrin@gmail.com>
2026-04-10 16:57:22 -0400

    implement Container Work

---
+++ BlogCoreSolution.DataAccess/Data/Repository/ContainerWork.cs
@@ -0,0 +1,25 @@
+using BlogCoreSolution.DataAccess.Data.Repository.IRepository;
+
+namespace BlogCoreSolution.DataAccess.Data.Repository;
+
+public class ContainerWork : IContainerWork
+{
+    private readonly ApplicationDbContext _dbContext;
+    public ContainerWork(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        Category = new CategoryRepository(_dbContext);
+    }
+    public ICategoryRepository Category { get; private set; }
+
+    public void Save()
+    {
+        _dbContext.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Dispose();
+    }
+
+}
